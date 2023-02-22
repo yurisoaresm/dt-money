b568a7164aee367241684c835ea8a6fb08fdd3da
Yuri Soares <yurisoaresm@outlook.com>
2023-02-22 09:41:33 -0300

    feat: create transactions HTTP request and context

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,13 @@
-import { SummaryCard, SummaryContainer } from "./styles";
+import { useContext } from "react";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+
+import { SummaryCard, SummaryContainer } from "./styles";
+
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext)
+  
   return (
     <SummaryContainer>
       <SummaryCard>

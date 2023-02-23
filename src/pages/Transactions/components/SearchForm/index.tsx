f0effbb66ee3ed564a624cbf1bc763cbbfd870a9
Yuri Soares <yurisoaresm@outlook.com>
2023-02-23 14:28:41 -0300

    feat: filter the search form results

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,7 +3,9 @@ import { useForm } from "react-hook-form";
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
+import { useContext } from "react";
 import { SearchFormContainer } from "./styles";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -12,6 +14,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext)
+
   const { 
     register, 
     handleSubmit,
@@ -21,9 +25,7 @@ export function SearchForm() {
   })
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    
-    console.log(data)
+    await fetchTransactions(data.query);
   }
 
   return (
